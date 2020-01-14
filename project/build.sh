0d99062eb753e1fa888df4910f3520dd573f65a3
Shishir <shishirlamichhane@gmail.com>
2020-01-14 16:21:04 +1100

    Upgrade of scala version to 2.12.10 such that applications upgraded to latest scala 2.12 binaries can seamlessly depend upon this library as the author is not maintaining this library and this is being used in production.

--- project/build.sh
+++ project/build.sh
@@ -2,8 +2,8 @@
 mkdir ./target/cli
 
 cp ./cli/src/main/scala/com/rockymadden/stringmetric/cli/**/*.scala ./target/cli/
-cp ./cli/target/scala-2.10/*.jar ./target/cli/
-cp ./core/target/scala-2.10/*.jar ./target/cli/
+cp ./cli/target/scala-2.12/*.jar ./target/cli/
+cp ./core/target/scala-2.12/*.jar ./target/cli/
 
 for f in ./target/cli/*.scala; do cat ./project/scala.sh "$f" > "${f%.*}"; done
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,1 @@
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "2.0.0")
